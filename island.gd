3ecff7b08f629fd162e2c0d8e00f4c1121c4aa66
spidunno <unnospid@gmail.com>
2024-09-03 17:51:03 -0400

    added static types for a lot of functions and vars

--- island.gd
+++ island.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 @export var spread: int = 32
 @export var startHeight: int = 0
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	if multiplayer.is_server():
 		randomize()
 		position = Vector3(randi_range(-spread*4,spread*4),startHeight,randi_range(-spread*4,spread*4))
